f162aa1d8fd65a317480b80f27fdfdaea1824413
[dyad] <git@dyad.sh>
2025-11-20 18:34:00 -0800

    [dyad] Corrigida a URL da API para a fonte de dados recomendada - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -38,7 +38,7 @@ const Index = () => {
     const fetchIcons = async () => {
       setIsLoading(true);
       try {
-        const response = await fetch('https://raw.githubusercontent.com/simple-icons/simple-icons/develop/data/simple-icons.json');
+        const response = await fetch('https://unpkg.com/simple-icons/icons.json');
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
