e35028197f7de0093beb646fd57abe86668c2bc9
[dyad] <git@dyad.sh>
2025-11-20 19:59:15 -0800

    [dyad] Corrigindo avisos de ref em componentes de paginação e avisos do React Router - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider delayDuration={0}>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
